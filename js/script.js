63e3ac588103ecdf0c26d8498799100a5b8560cb
Audrey POLVERINI <audrey.polverini@gmail.com>
2024-07-30 16:06:18 +0200

    update arrow left and right  position ok - addEventLister click ok, update dots : position ok, declaration JS ok

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
+// Button previous
+const btnPrev = document.getElementById("prev");
+btnPrev.addEventListener("click", function () {
+  console.log(btnPrev);
+});
+
+// Button next
+const btnNext = document.getElementById("next");
+btnNext.addEventListener("click", function () {
+  console.log(btnNext);
+});
+
+const dots = document.querySelectorAll(".dot");
+const image = document.querySelector(`.banner-img`);
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
